9eac684e2e1f244b44e75c634209956eca4b8ac2
shila <sdas47@my.centennialcollege.ca>
2019-01-26 12:00:07 -0500

    restructured project

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -1,1 +1,11 @@
 /*custom js here*/
+//IIFE-immediately invoked function express
+(function() {
+  function Start() {
+    console.log(
+      `%c App Started...`,
+      "font-size:20px;color:blue;font-weight:bold"
+    );
+  }
+  window.addEventListener("load", Start);
+})();
