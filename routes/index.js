8212b96d8161c9c07675b36eac0fa29bdd1c8a00
shila <sdas47@my.centennialcollege.ca>
2019-01-26 12:15:43 -0500

    fix active menu

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,18 @@ let router = express.Router();
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("index", { title: "Home" });
 });
-
+router.get("/about", function(req, res, next) {
+  res.render("index", { title: "About" });
+});
+router.get("/contact", function(req, res, next) {
+  res.render("index", { title: "Contact" });
+});
+router.get("/products", function(req, res, next) {
+  res.render("index", { title: "Products" });
+});
+router.get("/services", function(req, res, next) {
+  res.render("index", { title: "Services" });
+});
 module.exports = router;
