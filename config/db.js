91df7ca0ce9b1e2e69eb4a285638552314c66507
shila <sdas47@my.centennialcollege.ca>
2019-02-02 17:48:08 -0500

    add cloud mongo db

--- config/db.js
+++ config/db.js
@@ -1,3 +1,4 @@
 module.exports={
-    "URI" : "mongodb://localhost/demo"
+    //"URI" : "mongodb://localhost/demo"
+      "URI": "mongodb://admin:cen123@ds058739.mlab.com:58739/comp308-w2019-lession4a"
 }
