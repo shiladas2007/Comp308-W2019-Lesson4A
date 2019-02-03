8d5d4a35dfb6f6890869cdd759e06f97ec304d86
shila <sdas47@my.centennialcollege.ca>
2019-02-02 19:09:27 -0500

    add new db

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 module.exports={
     //"URI" : "mongodb://localhost/demo"
-      "URI": "mongodb://admin:cen123@ds058739.mlab.com:58739/comp308-w2019-lession4a"
+      "URI": "mongodb://admin:cen123@ds058739.mlab.com:58739/shila"
 }
